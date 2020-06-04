a7609fe6a5595a0aa11277fb4475a3ffd8d4a1f3
Afnan Hassan <50952934+HassanAfnan@users.noreply.github.com>
2020-06-04 23:45:13 +0500

    Add files via upload

---
+++ AboutUs.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks; 
+using System.Windows.Forms;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System;
+using System.IO;
+
+namespace frameworks
+{
+    public partial class AboutUs : MetroFramework.Forms.MetroForm
+    {
+        public AboutUs()
+        {
+            InitializeComponent();
+        }
+
+        private void AboutUs_Load(object sender, EventArgs e)
+        {
+            string filetext = File.ReadAllText(@"C:\Users\User\Desktop\oop Class work\amf.txt");
+            richTextBox1.Text = filetext;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
