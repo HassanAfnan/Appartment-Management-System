3119cc97dbdde2a09b09d89e18ec03d6ef0235f2
Afnan Hassan <50952934+HassanAfnan@users.noreply.github.com>
2020-06-05 00:05:27 +0500

    Add files via upload

---
+++ Details.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace frameworks
+{
+    partial class Details
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.crystalReportViewer4 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.c11 = new frameworks.reports.c1();
+            this.crystalReportViewer5 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.C21 = new frameworks.reports.C2();
+            this.crystalReportViewer6 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.C31 = new frameworks.reports.C3();
+            this.crystalReportViewer1 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.emp1 = new frameworks.reports.emp();
+            this.button1 = new System.Windows.Forms.Button();
+            this.crystalReportViewer2 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Bill1 = new frameworks.reports.Bill();
+            this.SuspendLayout();
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(293, 577);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "RESIDENT";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(391, 577);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 1;
+            this.button5.Text = "TENANT";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(497, 577);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 2;
+            this.button6.Text = "PARKING";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // crystalReportViewer4
+            // 
+            this.crystalReportViewer4.ActiveViewIndex = 0;
+            this.crystalReportViewer4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.crystalReportViewer4.Cursor = System.Windows.Forms.Cursors.Default;
+            this.crystalReportViewer4.Location = new System.Drawing.Point(13, 73);
+            this.crystalReportViewer4.Name = "crystalReportViewer4";
+            this.crystalReportViewer4.ReportSource = this.c11;
+            this.crystalReportViewer4.Size = new System.Drawing.Size(1048, 485);
+            this.crystalReportViewer4.TabIndex = 3;
+            // 
+            // crystalReportViewer5
+            // 
+            this.crystalReportViewer5.ActiveViewIndex = 0;
+            this.crystalReportViewer5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.crystalReportViewer5.Cursor = System.Windows.Forms.Cursors.Default;
+            this.crystalReportViewer5.Location = new System.Drawing.Point(13, 73);
+            this.crystalReportViewer5.Name = "crystalReportViewer5";
+            this.crystalReportViewer5.ReportSource = this.C21;
+            this.crystalReportViewer5.Size = new System.Drawing.Size(1048, 485);
+            this.crystalReportViewer5.TabIndex = 4;
+            // 
+            // crystalReportViewer6
+            // 
+            this.crystalReportViewer6.ActiveViewIndex = 0;
+            this.crystalReportViewer6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.crystalReportViewer6.Cursor = System.Windows.Forms.Cursors.Default;
+            this.crystalReportViewer6.Location = new System.Drawing.Point(13, 73);
+            this.crystalReportViewer6.Name = "crystalReportViewer6";
+            this.crystalReportViewer6.ReportSource = this.C31;
+            this.crystalReportViewer6.Size = new System.Drawing.Size(1048, 485);
+            this.crystalReportViewer6.TabIndex = 5;
+            // 
+            // crystalReportViewer1
+            // 
+            this.crystalReportViewer1.ActiveViewIndex = 0;
+            this.crystalReportViewer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.crystalReportViewer1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.crystalReportViewer1.Location = new System.Drawing.Point(13, 73);
+            this.crystalReportViewer1.Name = "crystalReportViewer1";
+            this.crystalReportViewer1.ReportSource = this.emp1;
+            this.crystalReportViewer1.Size = new System.Drawing.Size(1048, 485);
+            this.crystalReportViewer1.TabIndex = 6;
+            this.crystalReportViewer1.Load += new System.EventHandler(this.crystalReportViewer1_Load_1);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(602, 576);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "EMPLOYEE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // crystalReportViewer2
+            // 
+            this.crystalReportViewer2.ActiveViewIndex = 0;
+            this.crystalReportViewer2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.crystalReportViewer2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.crystalReportViewer2.Location = new System.Drawing.Point(13, 73);
+            this.crystalReportViewer2.Name = "crystalReportViewer2";
+            this.crystalReportViewer2.ReportSource = this.Bill1;
+            this.crystalReportViewer2.Size = new System.Drawing.Size(1048, 485);
+            this.crystalReportViewer2.TabIndex = 8;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(700, 575);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "BILL";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // Details
+            // 
+            this.ClientSize = new System.Drawing.Size(1084, 614);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.crystalReportViewer2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.crystalReportViewer1);
+            this.Controls.Add(this.crystalReportViewer6);
+            this.Controls.Add(this.crystalReportViewer5);
+            this.Controls.Add(this.crystalReportViewer4);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Name = "Details";
+            this.Text = "DETAILS";
+            this.TextAlign = MetroFramework.Forms.MetroFormTextAlign.Center;
+            this.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.Load += new System.EventHandler(this.Details_Load_1);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        //private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer1;
+        //private reports.CrystalReport2 CrystalReport21;
+        //private System.Windows.Forms.Button button1;
+        //private System.Windows.Forms.Button button2;
+        //private System.Windows.Forms.Button button3;
+        //private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer2;
+        //private reports.CrystalReport3 CrystalReport31;
+        //private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer3;
+        //private reports.CrystalReport4 CrystalReport41;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer4;
+        private reports.c1 c11;
+        private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer5;
+        private reports.C2 C21;
+        private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer6;
+        private reports.C3 C31;
+        private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer1;
+        private System.Windows.Forms.Button button1;
+        private reports.emp emp1;
+        private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer2;
+        private System.Windows.Forms.Button button2;
+        private reports.Bill Bill1;
+    }
+}
