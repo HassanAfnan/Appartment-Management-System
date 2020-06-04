8054995ccc626e5d4b2ba721e107922ccf741ac2
Afnan Hassan <50952934+HassanAfnan@users.noreply.github.com>
2020-06-05 00:16:29 +0500

    Add files via upload

---
+++ message.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace frameworks
+{
+    public partial class message : MetroFramework.Forms.MetroForm
+    {
+        public message()
+        {
+            InitializeComponent();
+        }
+
+        private void message_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
